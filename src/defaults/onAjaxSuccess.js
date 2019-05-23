ed0a5781ac2bacc1a8b9f323b568d8c565c72812
Duc Doan <ducdhm@gmail.com>
2019-05-23 17:34:50 +0700

    v1.0.4 - Fix issue alert message when ajax is error or success

--- src/defaults/onAjaxSuccess.js
+++ src/defaults/onAjaxSuccess.js
@@ -1,7 +1,7 @@
 let timer;
 
 export default (resp, form, options) => {
-    form.niceform('showSuccessMessage', resp.message || options.successMessage);
+    form.niceform('showSuccessMessage', resp.message || options.locale.successMessage);
     
     clearTimeout(timer);
     timer = setTimeout(() => form.niceform('hideMessage'), 5000);
