2d8bca686e1fe183ff8b5dbc8044bd5a985e6144
Duc Doan <ducdhm@gmail.com>
2020-03-17 17:00:02 +0700

    Update

--- tools/publishNewVersion/index.js
+++ tools/publishNewVersion/index.js
@@ -12,26 +12,30 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
 (async () => {
-    console.log(`-> Current version: "${currentVersion}" - new version: "${newVersion}"`);
+    try {
+        console.log(`-> Current version: "${currentVersion}" - new version: "${newVersion}"`);
 
-    console.log(`-> Update version: "${newVersion}" to "package.json"`);
-    packageJson.version = newVersion;
-    fs.writeFileSync('./package.json', JSON.stringify(packageJson, ' ', 4), 'utf-8');
+        console.log(`-> Update version: "${newVersion}" to "package.json"`);
+        packageJson.version = newVersion;
+        fs.writeFileSync('./package.json', JSON.stringify(packageJson, ' ', 4), 'utf-8');
 
-    console.log(`-> Build source-code`);
-    await exec(`npm run build`);
+        console.log(`-> Build source-code`);
+        await exec(`npm run build`);
 
-    console.log(`-> Push to github with message "v${newVersion}"`);
-    await exec(`git add .`);
-    await exec(`git commit -m "v${newVersion}"`);
-    await exec(`git push origin master`);
+        console.log(`-> Push to github with message "v${newVersion}"`);
+        await exec(`git add .`);
+        await exec(`git commit -m "v${newVersion}"`);
+        await exec(`git push origin master`);
 
-    console.log(`-> Create new release with message "Release of version ${newVersion}"`);
-    await exec(`git tag -a v${newVersion} -m "Release of v${newVersion}"`);
-    await exec('git push --tags');
+        console.log(`-> Create new release with message "Release of version ${newVersion}"`);
+        await exec(`git tag -a v${newVersion} -m "Release of v${newVersion}"`);
+        await exec('git push --tags');
 
-    console.log(`-> Publish version "${newVersion}" to "https://www.npmjs.com/"`);
-    await exec('npm publish');
+        console.log(`-> Publish version "${newVersion}" to "https://www.npmjs.com/"`);
+        await exec('npm publish');
 
-    console.log(`-> "${newVersion}" is published!`);
+        console.log(`-> "${newVersion}" is published!`);
+    } catch (e) {
+        console.log(`ERROR: ${e}`);
+    }
 })();
