11b28c585f0b6563db14479521db2cc885a17cde
Duc Doan <ducdhm@gmail.com>
2020-05-14 23:23:31 +0700

    Update

--- tools/publishNewVersion/index.js
+++ tools/publishNewVersion/index.js
@@ -1,36 +1,33 @@
 const fs = require('fs');
 const packageJson = require('../../package');
+const { increaseVersionNumber, getLogger } = require('@meenjs/utils');
 const currentVersion = packageJson.version;
-let newVersion = ((+currentVersion.replace(/\./g, '') + 1) + '').replace(/^(\d+)(\d)(\d)$/g, '$1.$2.$3');
-if (newVersion.length < 2) {
-    newVersion = `0.0.${newVersion}`;
-} else if (newVersion.length < 3) {
-    newVersion = `0.${newVersion}`;
-}
+const newVersion = increaseVersionNumber(currentVersion);
+const logger = getLogger(true);
 
-const { execSync} = require('child_process');
+const { execSync } = require('child_process');
 
 (() => {
-    console.log(`-> Current version: "${currentVersion}" - new version: "${newVersion}"`);
+    logger.info(`Current version: "${currentVersion}" - new version: "${newVersion}"`);
 
-    console.log(`-> Update version: "${newVersion}" to "package.json"`);
+    logger.info(`Update version: "${newVersion}" to "package.json"`);
     packageJson.version = newVersion;
     fs.writeFileSync('./package.json', JSON.stringify(packageJson, ' ', 4), 'utf-8');
 
-    console.log(`-> Build source-code`);
+    logger.info(`Build source-code`);
     execSync(`npm run build`);
 
-    console.log(`-> Push to github with message "v${newVersion}"`);
+    logger.info(`Push to github with message "v${newVersion}"`);
     execSync(`git add .`);
     execSync(`git commit -m "v${newVersion}"`);
     execSync(`git push origin master`);
 
-    console.log(`-> Create new release with message "Release of version ${newVersion}"`);
+    logger.info(`Create new release with message "Release of version ${newVersion}"`);
     execSync(`git tag -a v${newVersion} -m "Release of v${newVersion}"`);
     execSync('git push --tags');
 
-    console.log(`-> Publish version "${newVersion}" to "https://www.npmjs.com/"`);
+    logger.info(`Publish version "${newVersion}" to "https://www.npmjs.com/"`);
     execSync('npm publish');
 
-    console.log(`-> "${newVersion}" is published!`);
+    logger.info(`"${newVersion}" is published!`);
 })();
