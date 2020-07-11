d5678bf688020a7ef77c5a5efc876cc0a8a59e1a
Duc Doan <ducdhm@gmail.com>
2020-07-11 10:42:38 +0700

    Fix required rule fail with radio and checkbox input

--- src/defaults/_showErrorField.js
+++ src/defaults/_showErrorField.js
@@ -1,24 +1,26 @@
 export default (form, field, message) => {
-    let formGroup = field.closest('.form-group');
-    let inputGroup = field.closest('.input-group');
-    let feedback = formGroup.find('.form-control-feedback');
-    
-    field.addClass('is-invalid');
-    formGroup.addClass('has-error');
-    
-    let errorMessageEl = formGroup.find('.nf-error-message');
-    if (errorMessageEl.length === 0) {
-        errorMessageEl = $(`<div class="nf-error-message text-danger small" style="display: none;"></div>`);
+    if (!field.attr('data-hide-message')) {
+        let formGroup = field.closest('.form-group');
+        let inputGroup = field.closest('.input-group');
+        let feedback = formGroup.find('.form-control-feedback');
 
-        if (inputGroup.length > 0) {
-            inputGroup.after(errorMessageEl);
-        } else if (feedback.length > 0) {
-            feedback.after(errorMessageEl)
-        } else {
-            field.after(errorMessageEl)
+        field.addClass('is-invalid');
+        formGroup.addClass('has-error');
+
+        let errorMessageEl = formGroup.find('.nf-error-message');
+        if (errorMessageEl.length === 0) {
+            errorMessageEl = $(`<div class="nf-error-message text-danger small" style="display: none;"></div>`);
+
+            if (inputGroup.length > 0) {
+                inputGroup.after(errorMessageEl);
+            } else if (feedback.length > 0) {
+                feedback.after(errorMessageEl)
+            } else {
+                field.after(errorMessageEl)
+            }
         }
+
+        errorMessageEl.html(message);
+        form.niceform('showElement', errorMessageEl);
     }
-    
-    errorMessageEl.html(message);
-    form.niceform('showElement', errorMessageEl);
 };
